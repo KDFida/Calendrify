1f7108729e68e66a49a49278f67e9f889f6f0d9a
Fida Khadim <fidakhadim@gmail.com>
2023-09-24 20:55:43 +0100

    top bar design completed

---
+++ src/components/top-bar/topBar.js
@@ -0,0 +1,27 @@
+import logo from '../../assets/CalendrifyLogo.svg';
+import './topBar.css';
+
+function topBar() {
+    return (
+        <>
+        <div className='navbar'>
+            <div className='navbar-content'>
+                <div className='logo'>
+                    <img src={logo} className="App-logo" alt="logo" />
+                </div>
+                <div className='options'>
+                    <p className='options-text'>Home</p>
+                    <p className='options-text'>About Us</p>
+                    <p className='options-text'>Contact Us</p>
+                    <p className='options-text'>Log in</p>
+                </div>
+            </div>
+        </div>
+        <div className='banner'>
+            <p className='banner-content'>Limited time offer: Calendrify is free to use. Don't miss out!</p>
+        </div>
+        </>
+    )
+}
+
+export default topBar
