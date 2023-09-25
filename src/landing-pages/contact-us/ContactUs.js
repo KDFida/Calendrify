9009cfed0d4e47fa08e350ee5dd3a4656d081144
Fida Khadim <fidakhadim@gmail.com>
2023-09-25 18:35:03 +0100

    Contact us page created, no content

---
+++ src/landing-pages/contact-us/ContactUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './contactUs.css';
+import TopBar from "../../components/top-bar/topBar";
+
+function ContactUs() {
+    return (
+        <>
+            <TopBar />
+        </>
+    )
+}
+
+export default ContactUs;
