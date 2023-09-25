58ad09a4f20292366784882ed9e782dd01860484
Fida Khadim <fidakhadim@gmail.com>
2023-09-25 18:32:27 +0100

    About us page created, no content

---
+++ src/landing-pages/about-us/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './aboutUs.css';
+import TopBar from "../../components/top-bar/topBar";
+
+function AboutUs() {
+    return (
+        <>
+            <TopBar />
+        </>
+    )
+}
+
+export default AboutUs;
