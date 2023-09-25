7f63d91918b35a6960be5749e68ac82584f898fa
Fida Khadim <fidakhadim@gmail.com>
2023-09-25 18:56:31 +0100

    Login page created, no content

---
+++ src/landing-pages/login/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './login.css';
+import TopBar from "../../components/top-bar/topBar";
+
+function Login() {
+    return (
+        <>
+            <TopBar />
+        </>
+    )
+}
+
+export default Login;
