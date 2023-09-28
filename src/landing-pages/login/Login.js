3053e281ab61a4199617b29ee161d0921d7cfa95
Fida Khadim <fidakhadim@gmail.com>
2023-09-28 21:29:20 +0100

    Basic UI for login page done

--- src/landing-pages/login/Login.js
+++ src/landing-pages/login/Login.js
@@ -3,8 +3,16 @@ import './login.css';
 
 function Login() {
     return (
-        <>
-        </>
+        <div className="login">
+            <div className="login-container">
+                <h1 className="login-title">Log In</h1>
+                <form className="login-form">
+                    <input id="email" type="email" placeholder="Email" />
+                    <input id="password" type="password" placeholder="Password" />
+                    <button id="submit-button" type="submit">Sign In</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
