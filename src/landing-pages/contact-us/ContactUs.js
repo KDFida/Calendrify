d0bda49e261ce21ccada756a8f9207660bbe2b5a
Fida Khadim <fidakhadim@gmail.com>
2023-10-16 16:11:56 +0100

    contact us page form ui completed

--- src/landing-pages/contact-us/ContactUs.js
+++ src/landing-pages/contact-us/ContactUs.js
@@ -3,8 +3,16 @@ import './contactUs.css';
 
 function ContactUs() {
     return (
-        <>
-        </>
+        <div className="contact-container">
+            <h1 className="title">Contact Us</h1>
+            <form className="contact-form">
+                <input id="name" type="text" placeholder="Full Name" required />
+                <input id="email" type="email" placeholder="Email Address" required />
+                <textarea id="message" placeholder="Write your message..." required />
+
+                <button type="submit">Send Message</button>
+            </form>
+        </div>
     )
 }
 
