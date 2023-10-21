cec03ec7378db4be9c3fb3572b04a156bff4c653
Fida Khadim <fidakhadim@gmail.com>
2023-10-21 14:25:54 +0100

    firebase connected to login feature

--- src/landing-pages/user-account/login/Login.js
+++ src/landing-pages/user-account/login/Login.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import './login.css';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPasword] = useState("");
+
+    const authentication = getAuth();
+    const redirectTo = useNavigate();
+
+    const signIn = (e) => {
+        e.preventDefault();
+
+        signInWithEmailAndPassword(authentication, email, password)
+        .then((userCredentials) => { 
+            redirectTo('/app/home');
+        })
+        .catch((error) => {
+        })
+    }
+
     return (
         <div className="login">
             <div className="login-container">
                 <h1 className="login-title">Log In</h1>
                 <form className="login-form">
-                    <input id="email" type="email" placeholder="Email" />
-                    <input id="password" type="password" placeholder="Password" />
+                    <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+                    <input id="password" type="password" value={password} onChange={(e) => setPasword(e.target.value)} placeholder="Password" />
                     <Link className="forgot-password" to="/forgot-password">Forgot Password?</Link>
-                    <button id="submit-button" type="submit">Sign In</button>
+                    <button id="submit-button" type="submit" onClick={signIn}>Sign In</button>
                     <p className="create-account"> Not registered? <Link to="/create-account">Sign Up</Link></p>
                 </form>
             </div>
