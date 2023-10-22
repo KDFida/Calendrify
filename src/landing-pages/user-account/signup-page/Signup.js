f3a8705f113aee5340ade9bf5865116ba3f2966f
Fida Khadim <fidakhadim@gmail.com>
2023-10-22 07:36:19 +0100

    password validation

--- src/landing-pages/user-account/signup-page/Signup.js
+++ src/landing-pages/user-account/signup-page/Signup.js
@@ -14,8 +14,41 @@ function Signup() {
     const authentication = getAuth();
     const redirectTo = useNavigate();
 
+    const passwordValidation = (password, confirmPwd) => {
+        if (password !== confirmPwd) {
+            return "Passwords do not match. Please try again!";
+        } 
+
+        if (password.length < 8) {
+            return "Password must contain at least 8 characters. Please try again!";
+        }
+
+        if (!/[A-Z]/.test(password)) {
+            return "Password must contain at least one upper case character. Please try again!";
+        }
+
+        if (!/[a-z]/.test(password)) {
+            return "Password must contain at least one lower case character. Please try again!";
+        }
+
+        if (!/[0-9]/.test(password)) {
+            return "Password must contain at least one numeric character. Please try again!";
+        }
+
+        if (!/[!@#?$%&]/.test(password)) {
+            return "Password must contain at least one special character (!@#?$%&). Please try again!";
+        }
+
+        return null;
+    }
+
     const signUp = (e) => {
         e.preventDefault();
+
+        const passwordCheck = passwordValidation(password, confirmPwd);
+        if (passwordCheck !== null) {
+            return;
+        }
 
         createUserWithEmailAndPassword(authentication, email, password)
         .then((userCredentials) => {
