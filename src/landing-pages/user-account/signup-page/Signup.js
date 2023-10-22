47eb7abe93ff8f9e6c8cbc4cc9963929ca22a8d2
Fida Khadim <fidakhadim@gmail.com>
2023-10-22 07:19:17 +0100

    Firebase linked to create an account page

--- src/landing-pages/user-account/signup-page/Signup.js
+++ src/landing-pages/user-account/signup-page/Signup.js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState} from "react";
 import './signup.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import firebase from "../../../firebase/firebase";
+import { setDoc, doc } from "firebase/firestore";
 
 function Signup() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPasword] = useState("");
+    const [confirmPwd, setConfirmPwd] = useState("");
+
+    const authentication = getAuth();
+    const redirectTo = useNavigate();
+
+    const signUp = (e) => {
+        e.preventDefault();
+
+        createUserWithEmailAndPassword(authentication, email, password)
+        .then((userCredentials) => {
+            const uid = userCredentials.user.uid;
+            let accountCreationDate = new Date().toLocaleDateString();
+            return setDoc(doc(firebase.database, "Users", uid), {
+                uid: uid,
+                name: name,
+                email: email,
+                creationDate: accountCreationDate
+            });
+        })
+        .then(() => {
+            redirectTo('/login');
+        })
+        .catch((error) => {
+            
+        })
+    }
+
     return (
         <div className="signup">
             <div className="signup-container">
                 <h1 className="signup-title">Create an Account</h1>
                 <form className="signup-form">
-                    <input id="name-input" type="text" placeholder="Full Name"></input>
-                    <input id="email-input" type="email" placeholder="Email"></input>
-                    <input id="password-input" type="password" placeholder="Password"></input>
-                    <input id="confirm-password-input" type="password" placeholder="Confirm Password"></input>
-                    <button id="submit-button" type="submit">Create Account</button>
+                    <input id="name-input" type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Full Name"></input>
+                    <input id="email-input" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
+                    <input id="password-input" type="password" value={password} onChange={(e) => setPasword(e.target.value)} placeholder="Password"></input>
+                    <input id="confirm-password-input" type="password" value={confirmPwd} onChange={(e) => setConfirmPwd(e.target.value)} placeholder="Confirm Password"></input>
+                    <button id="submit-button" type="submit" onClick={signUp}>Create Account</button>
                     <p className="already-registered"> Already registered? <Link to="/login">Sign In</Link></p>
                 </form>
             </div>
