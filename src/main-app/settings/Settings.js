ad7dc1cb7b575f7656057dffd19e27b1fdc4ac62
Fida Khadim <fidakhadim@gmail.com>
2023-11-15 21:55:12 +0000

    Settings page started with sidebar

---
+++ src/main-app/settings/Settings.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './settings.css';
+import Sidebar from "../../components/sidebar/Sidebar";
+
+function Settings() {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Settings;
