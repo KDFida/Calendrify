865ff53f77a40d27a3fdb56f5e3bf93b4f3cceab
Fida Khadim <fidakhadim@gmail.com>
2023-11-15 21:58:15 +0000

    account page created with sidebar

---
+++ src/main-app/account/Account.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './account.css';
+import Sidebar from "../../components/sidebar/Sidebar";
+
+function Account() {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Account;
