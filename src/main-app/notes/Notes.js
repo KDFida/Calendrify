7aed67da80273bdb6a15ee2d22cd071448be4c89
Fida Khadim <fidakhadim@gmail.com>
2023-11-19 14:12:37 +0000

    Improved add note button UI

--- src/main-app/notes/Notes.js
+++ src/main-app/notes/Notes.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './notes.css';
+import { FaPlus } from "react-icons/fa";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 function Notes() {
     return (
         <div className="notes">
             <Sidebar />
-            <button className="add-note-button">Add New Note</button>
+            <button className="add-note-button">
+                <FaPlus className="newNote-icon"></FaPlus>
+                <span className="button-text">Add New Note</span>
+            </button>
         </div>
     )
 }
