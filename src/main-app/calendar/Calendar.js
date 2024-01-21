3562c800aaf58965403071e8535b92e418ce9582
Fida Khadim <fidakhadim@gmail.com>
2024-01-21 16:37:54 +0000

    Buttons created, need to make it responsive

--- src/main-app/calendar/Calendar.js
+++ src/main-app/calendar/Calendar.js
@@ -1,11 +1,22 @@
 import React from "react";
 import './calendar.css';
 import Sidebar from "../../components/sidebar/Sidebar";
+import { AiFillPlusCircle } from "react-icons/ai";
 
 function Calendar() {
     return (
-        <div>
+        <div className="calendar">
             <Sidebar />
+            <div className="top-bar">
+                <p className="title">Calendar</p>
+                <div className="buttons">
+                    <button className="availabilityButton">Set Availability</button>
+                    <button className="regenerateButton">New timetable</button>
+                    <button className="addTaskButton">                        
+                        <AiFillPlusCircle size={40} color="#09043d" />
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
