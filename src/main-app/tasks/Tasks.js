ecc0210175411405d190370c403f2f756cdc7987
Fida Khadim <fidakhadim@gmail.com>
2024-01-25 13:53:47 +0000

    add task in task page

--- src/main-app/tasks/Tasks.js
+++ src/main-app/tasks/Tasks.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import './tasks.css';
 import Sidebar from "../../components/sidebar/Sidebar";
+import { AiFillPlusCircle } from "react-icons/ai";
+import AddTaskDialog from "../components/add-task/AddTaskDialog";
 
 function Tasks() {
+    const [isDialogOpen, setDialogOpen] = useState(false);
+
+    const handleAddTask = () => {
+        setDialogOpen(true);
+      };
+    
+      const handleCloseDialog = () => {
+        setDialogOpen(false);
+      };
+
     return (
-        <div>
+        <div className="tasks-page">
             <Sidebar />
+            <div className="tasks-container">
+                <div className="top-bar">
+                    <p className="task-title">Tasks</p>
+                    <div className="buttons">
+                        <button className="addTaskButton" onClick={handleAddTask}>                        
+                            <AiFillPlusCircle size={40} color="#09043d" />
+                        </button>
+                    </div>
+                </div>
+                <AddTaskDialog open={isDialogOpen} onClose={handleCloseDialog}/>
+            </div>
         </div>
     )
 }
